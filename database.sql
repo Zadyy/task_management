c9f2f3b685b444866a81b02cc3f79dd2ecf0362d
Zadyy <nyambayar2014@gmail.com>
2024-05-09 11:50:34 +0800

    database initial creation, /constraint, connection incomplete/

--- database.sql
+++ database.sql
@@ -3,6 +3,31 @@ CREATE TABLE IF NOT EXISTS task_status (
     name varchar(20)
 );
 
+CREATE TABLE IF NOT EXISTS user (
+    id INT,
+    username VARCHAR(64),
+    password VARCHAR(64),
+    email VARCHAR(320),
+    created_at TIMESTAMP,
+    phone_no CHAR(8)
+);
+
+CREATE TABLE IF NOT EXISTS role (
+    id int,
+    name varchar(100)
+);
+
+CREATE TABLE IF NOT EXISTS department (
+    id int,
+    name VARCHAR(100),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS staff_status (
+    id int,
+    name VARCHAR(100)
+);
+
 CREATE TABLE IF NOT EXISTS task (
     id INT,
     title VARCHAR(200),
@@ -12,14 +37,20 @@ CREATE TABLE IF NOT EXISTS task (
     solution VARCHAR(4000)
 );
 
+CREATE TABLE IF NOT EXISTS staff (
+    id INT,
+    department_id int,
+    role_id int,
+    status TINYINT,
+    username VARCHAR(64),
+    password VARCHAR(64),
+    date_of_enrollment TIMESTAMP,
+    phone_no CHAR(8),
+    email VARCHAR(320)
+);
+
 CREATE TABLE IF NOT EXISTS task_assignment (
     task_id int,
     creator_id int,
     staff_id int
 );
-
-CREATE TABLE IF NOT EXISTS user (
-    id INT,
-    username VARCHAR(64),
-    
-)
